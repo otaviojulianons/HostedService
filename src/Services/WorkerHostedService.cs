2553442e2a034c3af2df4e14f871d3452db5886b
otaviojns <otavio.juliano.ns@gmail.com>
2019-08-10 12:37:59 -0300

    Ajustado para gerar delay randomico

--- src/Services/WorkerHostedService.cs
+++ src/Services/WorkerHostedService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Prometheus;
 using Serilog;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,23 +10,26 @@ namespace HostedService
     public class WorkerHostedService : HostedService
     {
         private readonly ILogger<WorkerHostedService> _logger;
+        private readonly Random _random;
         private readonly Histogram _workDuration;
 
         public WorkerHostedService(ILogger<WorkerHostedService> logger)
         {
             _logger = logger;
-            _workDuration = Metrics.CreateHistogram("worker_service_duration_seconds", "Histogram of worker processing durations.");
+            _random = new Random();
+            _workDuration = Metrics.CreateHistogram("worker_service", "Histogram of worker processing durations.");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stopToken)
         {
             _logger.LogInformation("ExecuteAsync-Begin");
+            
             while (!stopToken.IsCancellationRequested)
             {
                 using (_workDuration.NewTimer())
                 {
                     Log.Information("I'm alive");
-                    await Task.Delay(5000);
+                    await Task.Delay(_random.Next(100,1500));
                 }
             }
             _logger.LogInformation("ExecuteAsync-End");
