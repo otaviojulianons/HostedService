b31b22b2f819c96119fbd9bd0d0a69594aae211f
Otávio Nielsen <otavio.juliano.ns@gmail.com>
2019-11-18 21:44:26 -0300

    adicionado configurações de tempo de espera

--- src/Services/WorkerHostedService.cs
+++ src/Services/WorkerHostedService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Prometheus;
 using Serilog;
 using System;
@@ -10,12 +11,14 @@ namespace HostedService
     public class WorkerHostedService : HostedService
     {
         private readonly ILogger<WorkerHostedService> _logger;
+        private readonly IConfiguration _configuration;
         private readonly Random _random;
         private readonly Histogram _workDuration;
 
-        public WorkerHostedService(ILogger<WorkerHostedService> logger)
+        public WorkerHostedService(ILogger<WorkerHostedService> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
             _random = new Random();
             _workDuration = Metrics.CreateHistogram("worker_service", "Histogram of worker processing durations.");
         }
@@ -23,13 +26,15 @@ namespace HostedService
         protected override async Task ExecuteAsync(CancellationToken stopToken)
         {
             _logger.LogInformation("ExecuteAsync-Begin");
-            
+            var minDelay = _configuration.GetValue<int>("MinServiceDelay");
+            var maxDelay = _configuration.GetValue<int>("MaxServiceDelay");
+
             while (!stopToken.IsCancellationRequested)
             {
                 using (_workDuration.NewTimer())
                 {
                     Log.Information("I'm alive");
-                    await Task.Delay(_random.Next(100,1500));
+                    await Task.Delay(_random.Next(minDelay, maxDelay));
                 }
             }
             _logger.LogInformation("ExecuteAsync-End");
